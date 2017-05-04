431d2e8edb1dfaeef7b1e26c1a774911f979803f
Mati ZejnoV <mati.szwaba@gmail.com>
2017-05-04 11:52:22 +0200

    Created classes Journal.cs & Student.cs

---
+++ CourseJournalMS/CourseJournalMS/Journal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseJournalMS
+{
+    class Journal
+    {
+        public string CourseName;
+        public string CourseLeaderName, CourseLeaderSurname;
+        public DateTime CourseStartDate;
+        public double CourseHomeworkThreshold;
+        public double CoursePresenceThreshold;
+        public int CourseStudentsNumber;
+
+        public Dictionary<int,Student> CourseJournal = new Dictionary<int, Student>();
+
+    }
+}
---
+++ CourseJournalMS/CourseJournalMS/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseJournalMS
+{
+    class Student
+    {
+        public int OrderNumber;
+        public string Name, Surname;
+        public DateTime BirthDate;
+        public string Gender;
+
+    }
+}
