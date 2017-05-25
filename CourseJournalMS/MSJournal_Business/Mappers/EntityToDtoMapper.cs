6e434c748a8e4ec698dcafeed325abe4f7717c3e
Mati ZejnoV <mati.szwaba@gmail.com>
2017-05-25 11:56:15 +0200

    Przepychanki z Dto

---
+++ CourseJournalMS/MSJournal_Business/Mappers/EntityToDtoMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSJournal_Business.Dtos;
+using MSJournal_Data.Models;
+
+namespace MSJournal_Business.Mappers
+{
+    internal class EntityToDtoMapper
+    {
+        public static StudentDto StudentEntityModelToDto(Student student)
+        {
+            return new StudentDto()
+            {
+
+                
+            };
+        }
+
+        public static CourseDayDto CourseDayEntityModelToDto(CourseDay courseDay)
+        {
+            return new CourseDayDto()
+            {
+                
+            };
+        }
+
+    }
+}
